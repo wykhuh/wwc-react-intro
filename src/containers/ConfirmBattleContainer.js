632cff7f22dc0d10915685d2afa8eb945af3a4ad
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 05:26:26 -0700

    18. get query params for ConfirmBattleContainer

--- src/containers/ConfirmBattleContainer.js
+++ src/containers/ConfirmBattleContainer.js
@@ -2,6 +2,12 @@ var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
+  // after component loads, grab playerOne and playerTwo from url.
+  // then connect to github to get info for each player
+  componentDidMount: function() {
+    var query = this.props.location.query;
+    console.log('query', query);
+  },
 
   render: function() {
     return (
