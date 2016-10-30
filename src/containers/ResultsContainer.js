ee8844f375c3485c0f66a9a9ae8077fb66117600
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 09:24:27 -0700

    34. add githubHelpers.battle

--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var Results = require('../components/Results');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ResultsContainer = React.createClass({
   getInitialState: function() {
@@ -10,15 +11,20 @@ var ResultsContainer = React.createClass({
   },
 
   componentDidMount: function() {
-    // this.props.location.state.playersInfo
-    console.log(this.props)
+    githubHelpers.battle(this.props.location.state.playersInfo)
+      .then(function(scores){
+        this.setState({
+          scores: scores,
+          isLoading: false
+        })
+      }.bind(this))
   },
 
   render: function(){
     return(
       <Results
         isLoading={this.state.isLoading}
-        playersInfo ={this.props.location.state.playersInfo}
+        playersInfo={this.props.location.state.playersInfo}
         scores={this.state.scores}
       />
     )
--- src/utils/githubHelpers.js
+++ src/utils/githubHelpers.js
@@ -9,6 +9,44 @@ function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+// get repos for a user.
+// github api has a limit of retrieving 100 items.
+function getRepos(username){
+  return axios.get(
+    'https://api.github.com/users/' + username + '/repos' + param + '&per_page=100'
+  );
+}
+
+// get the sum of stars on all repos
+function getTotalStars(repos) {
+  return repos.data.reduce(function(prev, current){
+    return prev + current.stargazers_count;
+  }, 0);
+}
+
+// return an object that has followers and totalStars
+function getPlayersData(player) {
+  // player.login is the username
+  return getRepos(player.login)
+     // pass the repos from getRepos into getTotalStars
+    .then(getTotalStars)
+    .then(function(totalStars){
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+// return an array of scores.
+// players is an array of two objects. each object has follwers and totalStars
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function(players) {
     // call getUserInfo for each player.
@@ -24,6 +62,15 @@ var helpers = {
     .catch(function(err){
       console.warn('error in getPlayersInfo', err)
     })
+  },
+
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(err) { console.warn('Error in battle:', err)})
   }
 };
 
