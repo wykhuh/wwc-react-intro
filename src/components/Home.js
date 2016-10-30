b1e25a0113179537968a83367c34a2df7df0e313
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-29 21:09:29 -0700

    10. add styles and Link to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 var React = require('react');
 var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var transparentBg = require('../styles').transparentBg;
 
 var Home = React.createClass({
   render: function() {
     return(
-      <div className="jumbotron col-sm-12 text-center">
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
         <h1>Github Battle</h1>
         <p className='lead'>Who is the bigger geek?</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
       </div>
     )
   }
