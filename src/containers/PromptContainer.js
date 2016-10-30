0020827816bb1389640237896dfcd7eb8910f2cb
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-29 21:47:51 -0700

    13 redirect after submitting PromptContainer form

--- src/containers/PromptContainer.js
+++ src/containers/PromptContainer.js
@@ -2,6 +2,11 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+  // context lets you pass items to components without using props
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
   // user getInitialState to give a component state
   getInitialState: function() {
     // return an object that has all the state properties
@@ -18,13 +23,42 @@ var PromptContainer = React.createClass({
     })
   },
 
+  onSubmitUser: function(e) {
+    e.preventDefault();
+    // reset username
+    this.setState({
+      username: ''
+    })
+
+    // routeParams are the wildcard segments.
+
+    // if the url has a playerOne route params,
+    // go to /battle?playerOne=xxx&playerTwo=xxx
+    if(this.props.routeParams.playerOne) {
+      // router.push({pathname: xxx, query: xxx})
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+
+    // else go to playerTwo/:playerOne
+    } else {
+      // router.push(path)
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+
+  },
+
   render: function() {
     return(
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         {/* to access props pased into router, user this.props.route */}
         <h1>{this.props.route.header}</h1>
         <div className="col-sm-12">
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className="form-group">
               <input
                 type="text"
