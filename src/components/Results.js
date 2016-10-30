e2b26655d00c428ab13cbd556b3b1866c41a9b26
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 09:25:38 -0700

    35. add displayObj to Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,13 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
+function displayObj(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function Results(props) {
   return(
-    <div>Results</div>
+    <div>Results: {displayObj(props)}</div>
   )
 }
 
