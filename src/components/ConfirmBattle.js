6f058bfef01ecdcdf2e16c08ebdd7b16e35e1fee
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 08:18:51 -0700

    27. add basic template for ConfirmBattle

--- src/components/ConfirmBattle.js
+++ src/components/ConfirmBattle.js
@@ -1,14 +1,31 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function displayObj(object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var styles = require('../styles');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>Loading</p>
-    : <div>Confirm Battle: {displayObj(props)}</div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Confirm Players</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className="col-sm-6">
+          <p className="lead">Player 1</p>
+          Player 1 Info
+        </div>
+        <div className="col-sm-6">
+          <p className="lead">Player 2</p>
+          Player 2 Info
+        </div>
+      </div>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className='col-sm-12' style={styles.space}>
+          Intitiate Battle
+          </div>
+        <div className='col-sm-12' style={styles.space}>
+          Reselect Players
+        </div>
+      </div>
+    </div>
 }
 
 ConfirmBattle.propTypes = {
