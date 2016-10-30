96dcb872357cf369765c2a1a3d8cbb32bcbd8aaf
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 11:19:33 -0700

    36. add UserDetails to Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,30 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function displayObj(object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function Results(props) {
+  var winningIndex = 0;
+  var losingIndex = 1;
   return(
-    <div>Results: {displayObj(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails
+            score={props.scores[winningIndex]}
+            info={props.playersInfo[winningIndex]}
+          />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails
+            score={props.scores[losingIndex]}
+            info={props.playersInfo[losingIndex]}
+          />
+        </UserDetailsWrapper>
+      </div>
+    </div>
   )
 }
 
