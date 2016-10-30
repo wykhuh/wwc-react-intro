658c396c38a6c73037be5be0ea546ab154bf210b
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-29 21:08:34 -0700

    9. add content to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 var React = require('react');
+var ReactRouter = require('react-router');
 
 var Home = React.createClass({
   render: function() {
     return(
-      <div>Hello from Home</div>
+      <div className="jumbotron col-sm-12 text-center">
+        <h1>Github Battle</h1>
+        <p className='lead'>Who is the bigger geek?</p>
+      </div>
     )
   }
 })
