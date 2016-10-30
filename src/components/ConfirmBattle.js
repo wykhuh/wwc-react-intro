dec05399be2b7f2ef721afe353f4ed35426dcb88
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-30 08:01:50 -0700

    24. add displayObj to ConfirmBattle

--- src/components/ConfirmBattle.js
+++ src/components/ConfirmBattle.js
@@ -1,9 +1,12 @@
 var React = require('react');
+function displayObj(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>Loading</p>
-    : <p>Confirm Battle</p>
+    : <div>Confirm Battle: {displayObj(props)}</div>
 }
 
 module.exports = ConfirmBattle;
