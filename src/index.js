15df641d591e1b21b3527295f3a66fd339940c72
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-29 19:40:08 -0700

    1. create <HelloWorld />

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+var React = require('react');
+// React can work in various enviroment. Since we are making a webapp, use ReactDom
+var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// save the result of React.createClass into variable HelloWorld.
+// createClass will create a new component.
+var HelloWorld = React.createClass ({
+  // every component must have render
+  render: function() {
+    // return JSX.
+    return (
+      <div>Hello World!</div>
+    )
+  }
+});
+
+// render takes two parameters: component and an element in the DOM
+// render the component into the DOM
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
